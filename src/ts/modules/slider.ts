1ca55dbb6f5011a9685c62d87ed5cf970abc1a83
aliev008 <hfe.as@yandex.ru>
2022-11-03 01:14:27 +0300

    fix: types fix

--- src/ts/modules/slider.ts
+++ src/ts/modules/slider.ts
@@ -1,13 +1,13 @@
 export class Slider {
   selector: HTMLElement;
-  slides: any[];
+  slides: HTMLElement[];
   triggers: NodeListOf<HTMLElement>;
   slideIndex: number;
   numberOfSlides: number;
 
   constructor(selector: string, triggers: string) {
     this.selector = document.querySelector(selector);
-    this.slides = Array.from(this.selector.children);
+    this.slides = Array.from(this.selector.children as HTMLCollectionOf<HTMLElement>);
     this.triggers = document.querySelectorAll(triggers);
     this.slideIndex = 1;
     this.numberOfSlides = this.slides.length;
@@ -17,7 +17,7 @@ export class Slider {
     n > this.numberOfSlides && (this.slideIndex = 1);
     n < 1 && (this.slideIndex = this.numberOfSlides);
 
-    this.slides.forEach((slide: HTMLElement): void => {
+    this.slides.forEach((slide): void => {
       slide.style.display = "none";
       slide.classList.add("animated", "fadeIn");
     });
